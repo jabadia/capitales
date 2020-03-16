d2d83623d55901ce1e116655358a34555b7217fb
Javier Abadía <javi.abadia@gmail.com>
2020-03-16 23:27:22 +0100

    e2e tests? not convinced yet

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,11 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
-  })
-})
+    it('Visits the app root url', () => {
+        cy.visit('/');
+        cy.contains('button', 'Aceptar');
+        cy.contains('button', 'Volver a Empezar');
+        cy.get('.correct-score').should('contain', 'Aciertos: 0');
+        cy.get('.wrong-score').should('contain', 'Fallos: 0');
+    });
+});
