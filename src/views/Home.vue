5af6c0057c8ddc1fdf68d31f61963ef5cc4dab6d
Javier Abadía <javi.abadia@gmail.com>
2020-03-17 13:08:34 +0100

    some styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,46 @@
 <template>
-    <div>
-        <div>
-            <div>Intentos: {{attempts}}</div>
-            <div class="correct-score">Aciertos: {{correct}}</div>
-            <div class="wrong-score">Fallos: {{wrong}}</div>
-        </div>
-        <div>
-            <div v-for="attempt in history" :key="attempt.country">
-                {{attempt.country}}: {{attempt.capital}} <span>{{attempt.answerWasCorrect}}</span>
+    <div class="flex flex-row w-full">
+        <div class="p-2 w-1/2">
+            <div>
+                <div>Intentos: {{attempts}}</div>
+                <div class="correct-score">Aciertos: {{correct}}</div>
+                <div class="wrong-score">Fallos: {{wrong}}</div>
+            </div>
+            <div>
+                <div class="current-country border-2 border-gray-300 rounded-lg my-2 py-2 px-4 block w-full leading-normal text-center">{{currentCountry}}</div>
+                <div>
+                    <input
+                        class="bg-white focus:outline-none focus:shadow-outline border-2 border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal text-center"
+                        type="text" @keyup.enter="proceed" v-model='tryCapital' ref="theInput"/>
+                </div>
+            </div>
+            <div v-if="answerWasCorrect !== undefined" class="text-center py-2">
+                <div class="correct-answer text-green" v-if="answerWasCorrect === true">¡Correcto!</div>
+                <div class="wrong-answer text-red" v-if="answerWasCorrect === false">¡Incorrecto! era {{ currentCapital }}</div>
+            </div>
+            <div v-else class="text-center">
+                <button class="btn btn-blue m-2" @click="proceed">Aceptar</button>
+                <button class="btn btn-blue m-2" @click="reset">Volver a Empezar</button>
             </div>
         </div>
-        <div>
-            <div class="current-country">{{currentCountry}}</div>
-            <div><input type="text" @keyup.enter="proceed" v-model='tryCapital' ref="theInput"/></div>
-            <div class="correct-answer" v-if="answerWasCorrect === true">¡Correcto!</div>
-            <div class="wrong-answer" v-if="answerWasCorrect === false">¡Incorrecto! era {{ currentCapital }}</div>
-        </div>
-        <div v-if="answerWasCorrect === undefined">
-            <button @click="proceed">Aceptar</button>
-            <button @click="reset">Volver a Empezar</button>
+        <div class="py-2 px-8 w-1/2">
+            <table class="border-collapse table-fixed w-full">
+                <tbody>
+                <tr class="py-1" v-for="attempt in history" :key="attempt.country">
+                    <td class="px-1 w-1/3">
+                        {{attempt.country}}
+                    </td>
+                    <td class="px-1 w-1/3">
+                        {{attempt.capital}}
+                    </td>
+                    <td class="px-1">
+                        <span :class="attempt.answerWasCorrect? 'text-green' : 'text-red'">
+                            {{attempt.answerWasCorrect ? '✔' : '✖' }}
+                        </span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
@@ -80,6 +102,7 @@
                 this.wrong = 0;
                 this.correct = 0;
                 this.history = [];
+                this.pickRandom();
             },
             pickRandom() {
                 do {
@@ -98,3 +121,13 @@
         },
     };
 </script>
+
+<style>
+    .btn-blue {
+        @apply bg-blue text-white font-bold py-2 px-4 rounded
+    }
+
+    .btn-blue:hover {
+        @apply bg-blue-light
+    }
+</style>
